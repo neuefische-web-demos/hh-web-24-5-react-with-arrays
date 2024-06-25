de84c86bf56e7ee9884a67eff73649cc35bc72e1
Jessica Lörs <jessica.loers@gmail.com>
2024-06-25 10:18:24 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -26,19 +26,24 @@ const pokemons = [
   },
 ];
 
+// Array.map((singular) => { .... })
 export default function App() {
   return (
     <main className="app">
       <h1 className="app__title">Pokédex</h1>
       <ul className="app__pokemons">
-        <li>
-          <PokemonCard
-            id="448"
-            name="Lucario"
-            types="fighting, steel"
-            image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/448.svg"
-          />
-        </li>
+        {pokemons.map((pokemon) => {
+          return (
+            <li key={pokemon.id}>
+              <PokemonCard
+                name={pokemon.name}
+                types={pokemon.types}
+                image={pokemon.image}
+                id={pokemon.id}
+              />
+            </li>
+          );
+        })}
       </ul>
     </main>
   );
